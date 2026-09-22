afa60daad60a167c8d1d92080a2c369be0c4ec36
ankush <ankushmetareachmarketing@gmail.com>
2026-09-22 17:33:35 +0530

    remove un used files

--- auth.ts
+++ auth.ts
@@ -14,6 +14,7 @@ interface EmployeeAuthRow {
 }
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  trustHost: true,
   session: { strategy: "jwt" },
   pages: { signIn: "/login" },
   providers: [
